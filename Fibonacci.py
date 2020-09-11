33ab6d9a1fc3e8483f98cb99bfa71753895a65a5
Bravin Shumwe <64565230+shumwe@users.noreply.github.com>
2020-09-11 16:14:19 +0300

    Update and rename README.md to Fibonacci.py

---
+++ Fibonacci.py
@@ -0,0 +1,9 @@
+# Fibonacci_series
+Fibonacci series between 0 and 50.
+a  = 0
+x = 1
+
+while(x < 50):
+  print(x)
+  a = x + a
+  x + a
--- README.md
+++
@@ -1,2 +0,0 @@
-# Fibonacci_series
-Fibonacci series between 0 and 50.
